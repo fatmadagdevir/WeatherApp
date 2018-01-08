62fc69bf928f115b9c71fdd1f231d408d2947b81
Fatma Dağdevir <fatmadagdevir@fatmas-macbook-pro.local>
2018-01-08 09:43:50 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Fatma Dağdevir on 8.01.2018.
+//  Copyright © 2018 Fatma Dağdevir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
